50d865bb3402c175fa86e7127478d4fdf026e9cb
Jani <jani.swinnen@student.pxl.be>
2015-10-15 11:39:08 +0200

    Formatting fix
    Lege lijnen verwijderd/ toegevoegd voor de leesbaarheid

--- src/be/pxl/adapter/Dagger.java
+++ src/be/pxl/adapter/Dagger.java
@@ -4,8 +4,6 @@ package be.pxl.adapter;
  * Created by Jani Swinnen on 15/10/2015.
  */
 public class Dagger implements Knife{
-
-
     @Override
     public void stab() {
         System.out.println("Attack did 5 damage");
